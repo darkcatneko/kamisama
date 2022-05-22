d5582ac82eed25863e618113dd8bf82cd3665e63
darkcatneko <darkcat7126@gmail.com>
2022-05-22 14:55:22 +0800

    battle Prototype

--- Assets/scriptable object/SkillScriptableObject/Script/Skill.cs
+++ Assets/scriptable object/SkillScriptableObject/Script/Skill.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Skill : Skillbase
 {
-    public CoolSkill SkillFunction;
+    public UnityAction SkillAction;
     public SkillScriptableObject SkillInformation; 
 }
